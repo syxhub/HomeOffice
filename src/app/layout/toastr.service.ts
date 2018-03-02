aca8a35364c9855ca6fb0e95f64b5d2c485aba69
syxhub <miki@wanari.com>
2018-03-02 09:33:58 +0100

    12

--- src/app/layout/toastr.service.ts
+++ src/app/layout/toastr.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ToastrController } from 'ng2-toastr-notifications';
+// import { ToastrController } from 'ng2-toastr-notifications';
 
 @Injectable()
 export class ToastrService {
 
-  constructor(private toastCtrl: ToastrController) { }
+  // constructor(private toastCtrl: ToastrController) { }
 
   // showToast(type: string, title: string, message: string) {
   //   this.toastCtrl.show({ type: type, title: title, message: message, position: 'top-right' });
